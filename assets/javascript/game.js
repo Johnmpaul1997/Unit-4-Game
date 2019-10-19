d88dc07812b73e961944ce00502838064b1dee55
John Paul <johnmpaul1997@gmail.com>
2019-10-19 16:44:48 -0700

    added score for javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,5 +3,20 @@ console.log("hello")
 
 let randNum= Math.floor(Math.random() *((120 - 19) + 1) + 19);
 console.log(randNum)
-
 $("#randNum").html(randNum);
+
+
+    alex = Math.floor(Math.random() * 12) + 1;
+    console.log(alex)
+    diamond = Math.floor(Math.random() * 12) + 1;
+    console.log(diamond)
+    ruby = Math.floor(Math.random() * 12) + 1;
+    console.log(ruby)
+    sapphire = 1;
+    console.log(sapphire)
+
+    wins = 0; 
+    losses = 0; 
+
+    totScore = 0;
+    $('#score').text(totScore);
