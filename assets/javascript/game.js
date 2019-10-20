609b51a079fdbbed4ffde1aca7f5b6463653c147
John Paul <johnmpaul1997@gmail.com>
2019-10-19 21:36:44 -0700

    stuck on Btn jquery syntax. Unable to resolve problem moving to readme.md for turn in

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,57 @@
 console.log("hello")
 
 
-let randNum= Math.floor(Math.random() *((120 - 19) + 1) + 19);
+let randNum = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
 console.log(randNum)
 $("#randNum").html(randNum);
 
 
-    alex = Math.floor(Math.random() * 12) + 1;
-    console.log(alex)
-    diamond = Math.floor(Math.random() * 12) + 1;
-    console.log(diamond)
-    ruby = Math.floor(Math.random() * 12) + 1;
-    console.log(ruby)
-    sapphire = 1;
-    console.log(sapphire)
+alex = Math.floor(Math.random() * 12) + 1;
+console.log(alex)
+diamond = Math.floor(Math.random() * 12) + 1;
+console.log(diamond)
+ruby = Math.floor(Math.random() * 12) + 1;
+console.log(ruby)
+sapphire = 1;
+console.log(sapphire)
 
-    wins = 0; 
-    losses = 0; 
+lose = 0;
+win = 0;
 
-    totScore = 0;
-    $('#score').text(totScore);
+
+score = 0;
+$("#score").text(score);
+
+function win() {
+    win = win + 1;
+    $("#wins").html(wins);
+    reset();
+}
+
+function lose() {
+    lose = lose + 1;
+    $("#loss").html(wins);
+    reset();
+}
+
+$('#alex').click((){
+score = score + alex
+$("#currScore").text(score);
+if(score === randNum){
+    win();
+    reset();
+}
+else if (score > randNum){
+    lose();
+    reset();
+}
+
+});
+   
+//cant figure how to make the buttons add to my score. 
+//whenever i try it always pops up as a syntax error.
+// with the syntax error it removes my display of the current score.
+function reset() {
+    score = 0;
+    $('#score').text(score);
+}
